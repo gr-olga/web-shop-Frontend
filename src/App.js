8aa75e5a79f04fef1b3fd43ccee2ef795a6f26a8
Olga Panfilova <gr-olga@ya.ru>
2022-06-07 15:12:39 +0200

    create routes, pages, components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
+import {Route, Routes} from "react-router-dom";
 import './App.css';
+import HomePage from "./pages/HomePage/HomePage.jsx";
+import DetailsPage from "./pages/DetailsPage/DetailsPage";
+import ProductList from "./pages/ProductList/ProductList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Routes>
+                    <Route path='/' element={<HomePage/>}/>
+                    <Route path='/details/:id' element={<DetailsPage/>}/>
+                    <Route path='/shop' element={<ProductList/>}/>
+                </Routes>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/ProductList/ProductList.jsx
@@ -0,0 +1,7 @@
+export default function ProductList(){
+    return(
+        <div>
+            <h3>Products List</h3>
+        </div>
+    )
+}
