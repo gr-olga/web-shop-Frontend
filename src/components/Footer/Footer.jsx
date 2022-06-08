eaa1466dee93e6a5301e38021b6fef6d3886dae2
Olga Panfilova <gr-olga@ya.ru>
2022-06-08 15:20:31 +0200

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,29 @@
+import "./styles.css"
+export default function Footer(){
+    return(
+        <div>
+            <div>
+                <span>The</span><span>Shop</span>
+                <input/>
+                <button>Sign Up</button>
+            </div>
+            <div>
+                <title>Categories</title>
+                <nav>
+                    <li>Electronics</li>
+                    <li>Jewelery</li>
+                    <li>Men's Clothing</li>
+                </nav>
+            </div>
+            <div>
+                <title>My account</title>
+                <nav>
+                    <li>My account</li>
+                    <li>Jewelery</li>
+                    <li>Men's Clothing</li>
+                </nav>
+            </div>
+            <div></div>
+        </div>
+    )
+}
