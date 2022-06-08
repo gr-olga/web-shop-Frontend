4b1556ac010eff3bf0379a97be1296e8f5c9a372
Olga Panfilova <gr-olga@ya.ru>
2022-06-08 16:12:07 +0200

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,29 +1,41 @@
 import "./styles.css"
-export default function Footer(){
-    return(
-        <div>
-            <div>
-                <span>The</span><span>Shop</span>
-                <input/>
-                <button>Sign Up</button>
+import {BsFacebook, BsInstagram} from "react-icons/bs";
+import {AiFillTwitterCircle} from "react-icons/ai";
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <div className="signUp">
+                <span className="the">The</span><span className="shop">Shop</span>
+                <input className="input"/>
+                <button className="signUpBtn">Sign Up</button>
             </div>
-            <div>
-                <title>Categories</title>
-                <nav>
-                    <li>Electronics</li>
-                    <li>Jewelery</li>
-                    <li>Men's Clothing</li>
+            <div className="category">
+                <h3 className="title">Categories</h3>
+                <nav className="navBar">
+                    <li className="navElement">Electronics</li>
+                    <li className="navElement">Jewelery</li>
+                    <li className="navElement">Men's Clothing</li>
                 </nav>
             </div>
-            <div>
-                <title>My account</title>
-                <nav>
-                    <li>My account</li>
-                    <li>Jewelery</li>
-                    <li>Men's Clothing</li>
+            <div className="category">
+                <h3 className="title">My account</h3>
+                <nav className="navBar">
+                    <li className="navElement">My account</li>
+                    <li className="navElement">Discount</li>
+                    <li className="navElement">Returns</li>
+                    <li className="navElement">Orders History</li>
+                    <li className="navElement">Order Tracking</li>
                 </nav>
             </div>
-            <div></div>
+            <div className="shareBox">
+                <h3>Follow us</h3>
+                <div className="share-buttons">
+                    <BsFacebook className="share"/>
+                    <BsInstagram className="share"/>
+                    <AiFillTwitterCircle className="share"/>
+                </div>
+            </div>
         </div>
     )
 }
