91bdbc862c7b1015f5a7a5b715c5f848ec89416e
Olga Panfilova <gr-olga@ya.ru>
2022-06-08 12:29:08 +0200

    add detail page

--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -1,47 +1,64 @@
 import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import stars from "../../images/img.png"
-import {BsCartDash} from "react-icons/bs";
+import {BsCartDash, BsFacebook, BsInstagram} from "react-icons/bs";
+import {AiFillTwitterCircle, AiOutlineHeart} from "react-icons/ai"
 import axios from "axios";
+import './DetailsPage.css'
 
 export default function DetailsPage() {
     const params = useParams();
     const [product, setProduct] = useState(null);
 
     useEffect(() => {
-        async function getDetails() {
-            const res = await axios.get(
-                ` https://localhost:4000/products/${params.id} `
-            );
-            console.log(res.data);
-            setProduct(res.data);
+        try {
+            async function getDetails() {
+                const res = await axios.get(
+                    ` http://localhost:4000/products/${params.id} `
+                );
+                setProduct(res.data);
+            }
+
+            getDetails();
+        } catch (error) {
+            console.log(error.message);
         }
 
-        getDetails();
     }, []);
 
 
     return product ? (
-        <div>
-            <h5>Path: Home. Shop. {product.name}</h5>
             <div>
-                <img className='productIng' src={product.mainImage} alt={product.name}/>
-                <div>
-                    <h2 className='name'>{product.name}</h2>
+                <h5>Path: Home. Shop. {product.title}</h5>
+                <div className="detail_box">
                     <div>
-                        <img src={stars} alt={'rating'}/>
-                        <button>add review</button>
+                        <img className='productIng' src={product.mainImage} alt={product.title}/>
                     </div>
-                    <p>€ {product.price}</p>
-                    <h4>{product.description}</h4>
-                    <div>
-                        <button><BsCartDash/>Add To cart</button>
-                        <button></button>
+                    <div className="detail_description">
+                        <h2 className='name'>{product.title}</h2>
+                        <div className="rating">
+                            <img src={stars} alt={'rating'}/>
+                            <button>add review</button>
+                        </div>
+                        <p>€ {product.price}</p>
+                        <h4>{product.description}</h4>
+                        <div>
+                            <button className='add-button'><BsCartDash/> Add To cart</button>
+                            <button><AiOutlineHeart/> Favorite</button>
+                        </div>
+                        <h3>Category: {product.category.title}</h3>
+                        <div className="share-buttons">
+                            <h5 className="share">Share</h5>
+                            <BsFacebook className="share"/>
+                            <BsInstagram className="share"/>
+                            <AiFillTwitterCircle className="share"/>
+                        </div>
                     </div>
+
                 </div>
             </div>
-        </div>
-    ) : (
-        <p>Loading ...</p>
-    )
+        ) :
+        (
+            <p>Loading ...</p>
+        )
 }
